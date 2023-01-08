b4b98e88463bd81089f9de32e741fba03a4e1183
hyunwoo <ashyonu@gmail.com>
2023-01-09 00:17:46 +0900

    feat: add appspec, deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+REPOSITORY=/home/ec2-user/mathrone
+FRONT_REPO=/home/ec2-user/mathrone/MATHRONE_Frontend_Submodule
+BACK_REPO=/home/ec2-user/mathrone/MATHRONE_Backend_Submodule
+
+cd REPOSITORY
+echo ">실행"
+docker kill $(docker -ps -q)
+git submodule update --init --recursive
+cd FRONT_REPO
+git checkout "develop"
+git pull
+cd BACK_REPO
+git checkout "develop"
+git pull 
