1cecbcbdb4a3738ba753557bf3cce5a916e35297
hyunwoo <ashyonu@gmail.com>
2023-01-09 00:53:09 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,14 @@ REPOSITORY=/home/ec2-user/mathrone
 FRONT_REPO=/home/ec2-user/mathrone/MATHRONE_Frontend_Submodule
 BACK_REPO=/home/ec2-user/mathrone/MATHRONE_Backend_Submodule
 
-cd REPOSITORY
+cd $REPOSITORY
 echo ">실행"
 docker kill $(docker -ps -q)
-pwd
 git submodule update --init --recursive
-cd MATHRONE_Frontend_Submodule
+cd $FRONT_REPO
 git checkout "develop"
 git pull
 cd ..
-cd MATHRONE_Backend_Submodule
+cd $BACK_REPO
 git checkout "develop"
 git pull 
