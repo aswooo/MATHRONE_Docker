71a58cc07e52117d1a2c3c4fa6443327a07e885a
hyunwoo <ashyonu@gmail.com>
2023-04-20 22:58:26 +0900

    feat: asfd

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,4 @@
-REPOSITORY=/home/ec2-user/mathrone
-FRONT_REPO=/home/ec2-user/mathrone/MATHRONE_Frontend_Submodule
-BACK_REPO=/home/ec2-user/mathrone/MATHRONE_Backend_Submodule
-
+sudo docker-compose down --volumes
 cd /home/ec2-user/app
 sudo docker-compose up --build
-
-# cd $REPOSITORY
-# echo ">실행"
-# docker kill $(docker -ps -q)
-# sudo git submodule update --init --recursive
-# cd $FRONT_REPO
-# git checkout "master"
-# git pull
-# cd ..
-# cd $BACK_REPO
-# git checkout "master"
-# git pull 
+sudo rm -rf *
